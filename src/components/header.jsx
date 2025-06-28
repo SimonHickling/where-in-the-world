e2c855589a2170059f1165d5be94f89a0a09dd09
Simon Hickling <simon.hickling6@googlemail.com>
2025-06-28 09:24:56 +0100

    sun icon

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import './header.css'
 
 
+
 const Header = ({onToggleDark} ) => {
+      const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleToggle = () => {
+    setIsDarkMode(prev => !prev);
+    onToggleDark(); // Call parent toggle function
+  };
     return (
         <>
           <div className="header-bar">
                         <div className="site-title">
                             <h1>Where in the World?</h1>
-                            <button onClick={onToggleDark}>
-                                <img src="/images/moon-svgrepo-com.svg" alt="Dark Mode Icon"/>
-                                &nbsp;Dark Mode
+                            <button onClick={handleToggle}>
+                                 <img
+                                    src={isDarkMode ? "/images/sun-svgrepo-com.svg" : "/images/moon-svgrepo-com.svg"}
+                                    alt={isDarkMode ? "image" : "Dark Mode Icon"}
+                                />
+                                &nbsp;{isDarkMode ? "Light Mode" : "Dark Mode"}
                             </button>
                         </div>
                     </div>
